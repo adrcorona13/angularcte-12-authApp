b018378ffd10b1e61566b94be1c9b7385aab3332
Adrian Pineda <adrian.pc13@hotmail.com>
2023-03-07 22:32:54 -0600

    agregar routing

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+const routes: Routes = [
+    // {
+    //     path: 'auth',
+    //     loadChildren: 
+    // },
+    {
+        path: '**', redirectTo: 'auth'
+    }
+]
+@NgModule({
+    imports: [
+        RouterModule.forRoot(routes)
+    ], 
+    exports: [
+        RouterModule
+    ]
+})
+export class AppRoutingModule{}
